980b70bf948b8a316dfcce1aa01edaa85129f80a
Luis3nov <luis3novillo@gmail.com>
2026-03-23 12:44:48 +0000

    commit2

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -2,4 +2,5 @@
 gpg --list-secret-keys --keyid-format=long
 #exportamos mi llave privada y con armor la transforamrmos de binario a texto
 gpg --armor --export-secret-keys B84A19B106C03AD7
-#
+#exporto mi llave publica
+gpg --armor --export luis3novillo@gmail.com > mi_llave_publica.asc
