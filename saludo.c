186d03664a54af1753dd7ab04b61350f05d45ff7
Luis3nov <luis3novillo@gmail.com>
2026-03-25 13:01:34 +0000

    commit9

--- saludo.c
+++ saludo.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> //aqui estoy imortando una biblioteca
 
-int main() {
+int main() { //en c debo definir si o si que tipo de variable es
     printf("Hello World!\n");
-    return 0;
+    return 0; //y el return 0 sirve para ver si mi codigo funciono, si no funciono me da -1
 }
