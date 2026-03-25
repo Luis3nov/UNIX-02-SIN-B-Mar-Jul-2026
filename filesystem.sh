9eaa473f6ca92786030c6f7d9a04d9a5fad8c691
Luis3nov <luis3novillo@gmail.com>
2026-03-25 13:18:54 +0000

    commit10

--- filesystem.sh
+++ filesystem.sh
@@ -1,1 +1,4 @@
 pwd #print working directory
+cd/ #cd significa change directory
+ls -F #este comando lista los archivos y agrega símbolos al final de los nombres para indicar el tipo de archiv
+ls -i #y con este comando muestor los inodos de cada archivo
