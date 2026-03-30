0d49f8905032b07c08c184aed900c63cc147c69f
Luis3nov <luis3novillo@gmail.com>
2026-03-30 12:55:47 +0000

    ldu1

---
+++ practical_class2.sh
@@ -0,0 +1,4 @@
+
+sudo apt update #this command refresh the list of available software
+sudo apt upgrade #this install available updates
+sudo apt install parted #and this install the program that we need
