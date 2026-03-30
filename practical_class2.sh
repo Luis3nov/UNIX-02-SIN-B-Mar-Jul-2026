322e39457fb53966c7889005816c28e55b0822b9
Luis3nov <luis3novillo@gmail.com>
2026-03-30 13:13:48 +0000

    ldu3

--- practical_class2.sh
+++ practical_class2.sh
@@ -3,3 +3,4 @@ sudo apt update #this command refresh the list of available software
 sudo apt upgrade #this install available updates
 sudo apt install parted #and this install the program that we need
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n" #firt this command list all the disks en partitions and show disks in simple tree format, "&&" means AND, only print the next command if the preveously was correct
+[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #this means an if, if that cirecotry contanins UEFI print Uefi, else print BIOS. it also represents OR.
