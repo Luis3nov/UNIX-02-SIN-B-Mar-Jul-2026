0e9840deb145325114edf6e8a9dffdc08e4b4c6d
Luis3nov <luis3novillo@gmail.com>
2026-03-30 13:07:17 +0000

    ldu2

--- practical_class2.sh
+++ practical_class2.sh
@@ -2,3 +2,4 @@
 sudo apt update #this command refresh the list of available software
 sudo apt upgrade #this install available updates
 sudo apt install parted #and this install the program that we need
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n" #firt this command list all the disks en partitions and show disks in simple tree format, "&&" means AND, only print the next command if the preveously was correct
