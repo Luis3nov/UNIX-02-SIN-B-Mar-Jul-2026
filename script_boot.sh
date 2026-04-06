d1a5abe7b5085065369855f9c5b0eeb53397e9c8
Luis3nov <luis3novillo@gmail.com>
2026-04-06 12:39:14 +0000

    commit_one

---
+++ script_boot.sh
@@ -0,0 +1,4 @@
+cd #cd means change directory and it takes me to the root directory (home)
+cd /home/codespace #it takes me to an absolute directory with the principal folder in codespaces
+cd ~ #~ is a shortcut for home, so it takes me to the home directory
+cd $HOME #tt takes you to the directory that is stored in the HOME environment variable, like the two others
