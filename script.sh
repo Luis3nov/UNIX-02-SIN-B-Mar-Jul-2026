9da169a306f90314871a470ebc20ce083db75fb6
Luis3nov <luis3novillo@gmail.com>
2026-04-13 13:19:54 +0000

    ldu7

--- script.sh
+++ script.sh
@@ -12,3 +12,8 @@ ls -help #and this gives me explanations
 man ls #this is the complete manual
 
 --depth #Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless --no-single-branch is given to fetch the histories near the tips of all branches. If you want to clone submodules shallowly, also pass --shallow-submodules.
+
+chmod +x script.sh #It adds execution permission to script.sh.
+chmod u+x script.sh #It adds execution permission only to the file owner.
+chmod o-r secreto.txt #It removes read permission from other users.
+chmod u+rw,go-rwx privado #Only the owner will be able to read and write. No one else will have access.
