28cbcc285df5a2b11bf054a8938e05a56b230087
Luis3nov <luis3novillo@gmail.com>
2026-04-13 12:43:03 +0000

    ldu2

--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
 ls -a and ls --all #is the same
 ls -a / # it shows all the files
+ls -l #Shows detailed information about each file.
+ls -a #Lists all files, including hidden ones.
+ls -h #Lists all files, including hidden ones.
+ls -lah
