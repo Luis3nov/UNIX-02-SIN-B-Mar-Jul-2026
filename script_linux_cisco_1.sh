4ed5ef5e0aaed6b95c0ef21ebec739eedc934515
Luis3nov <luis3novillo@gmail.com>
2026-05-04 02:29:21 +0000

    commit1

---
+++ script_linux_cisco_1.sh
@@ -0,0 +1,10 @@
+ls #it list files and directories in the current working directory
+LS #"LS" is not the same as "ls", so this will produce an error
+ls documents #list contents of a specific directory (Documents)
+ls -l #Display a long (detailed) listing of files and directories
+ls -r # List directory contents in reverse alphabetical order
+ls -rl #combine options
+aptitude moo #Run the aptitude package manager with a special "moo" argument
+aptitude -v moo # Add verbose option (-v) to modify command behavior
+aptitude -vv moo # Each additional "v" changes the output further, the easter egg is in the vvvvv
+pwd #it Print the current working directory
