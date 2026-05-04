2c5de1a8175f1709292753f5a11021b6790ecec8
Luis3nov <luis3novillo@gmail.com>
2026-05-04 13:38:19 +0000

    commitmayo2

--- security.sh
+++ security.sh
@@ -3,3 +3,13 @@ cat /etc/passwd | head -10 #display first 10 lines of /etc/passwd
 cat /etc/group | head -10 #display first 10 lines of /etc/group
 groups #it shows the groups of the atual user
 groups $USER #it do the same of the previous
+
+id -u #show current user id
+id -g #show current user primary GID
+id -G #show all group IDs for current user
+
+cat /etc/group | grep games #displays the group entry for “games” from the system groups file
+cat /etc/gshadow #displays the secure group file, including group passwords and admin members
+
+mkdir ~/proyecto_unix
+ls -la ~/proyecto_unix/
