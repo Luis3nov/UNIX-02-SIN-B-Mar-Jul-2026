6622271d22be43520b4a09df7c5fa938fa67419c
Luis3nov <luis3novillo@gmail.com>
2026-05-04 13:31:07 +0000

    commitmayo1

---
+++ security.sh
@@ -0,0 +1,5 @@
+id # show current user and groups
+cat /etc/passwd | head -10 #display first 10 lines of /etc/passwd
+cat /etc/group | head -10 #display first 10 lines of /etc/group
+groups #it shows the groups of the atual user
+groups $USER #it do the same of the previous
