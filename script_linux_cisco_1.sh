319732d6e269c5066a9d5049ab824b277efc0ef2
Luis3nov <luis3novillo@gmail.com>
2026-05-04 02:46:57 +0000

    commit2

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -8,3 +8,16 @@ aptitude moo #Run the aptitude package manager with a special "moo" argument
 aptitude -v moo # Add verbose option (-v) to modify command behavior
 aptitude -vv moo # Each additional "v" changes the output further, the easter egg is in the vvvvv
 pwd #it Print the current working directory
+
+cd Documents # Change the current working directory to the Documents directory
+cd / # Change the current working directory to the root directory
+cd /home/sysadmin
+cd .. # Move one level up to the parent directory
+cd ~ # Change to the current user's home directory
+ls -l /var/log/ # Display a long detailed listing of the /var/log directory
+ls -lt /var/log # The newest modified files are shown first
+ls -l -S /var/log # The largest files are shown first
+ls -lSr /var/log # The smallest files are shown first
+su - # Switch to another user account using a login shell
+exit # Exit the current shell session
+sl # Run the sl command, in this case it needs sudo
