0b1fe2fc24827701ace354541e43a17f218cb26b
Luis3nov <luis3novillo@gmail.com>
2026-05-06 12:26:04 +0000

    commitmayo4

--- security.sh
+++ security.sh
@@ -14,3 +14,7 @@ cat /etc/gshadow #displays the secure group file, including group passwords and 
 mkdir ~/proyecto_unix #it creates a new directory: proyecto_unix
 ls -la ~/proyecto_unix/ #it list all files with detailed information included hidden files
 
+#groupadd [opciones] nombre_grupo
+sudo groupadd desarrolladores #here we create a simple group
+groupadd -g 2000 operaciones #it creates a group named operaciones with the custom GID 2000
+groupadd --system servicios_web #creates a system group named servicios_web
