9be28f57c68ec613704b3ed4e9f96c009e97ee64
Luis3nov <luis3novillo@gmail.com>
2026-05-27 13:00:52 +0000

    commmitmayo27

---
+++ security2.sh
@@ -0,0 +1,4 @@
+id 
+id -gn # to see main group
+touch ~/test_grupo_heredado.txt #here we create a fil
+ls -la ~/test_grupo_heredado.txt #here we see the group come into the file, that is the main group
